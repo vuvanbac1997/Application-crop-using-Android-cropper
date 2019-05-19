d03a2301a34b2bc20a4a89d12dfca77d4a7e78fe
bac_vu <vuvan.chanqua@gmail.com>
2019-05-19 20:24:16 +0700

    update image

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
-"# Application-crop-using-Android-cropper" 
+# Ứng dụng crop ảnh sử dụng thư viện Android-Image-Cropper
+
+## Demo ứng dụng
+
+1. Giao diện
+	![anh_1](demo/anh_1.png)
+2. Chọn từ các nguồn khác nhau
+	![anh_2](demo/anh_2.png)
+3. Chọn ảnh
+	![anh_3](demo/anh_3.png)
+4. Xác nhận chọn ảnh
+	![anh_4](demo/anh_4.png)
+5. Bắt đầu crop. Có thể rotate
+	![anh_5](demo/anh_5.png)
+6. Chọn lưu để lưu vào điện thoại
+	![anh_6](demo/anh_6.png)
+7. Ảnh đã lưu trong album mới
+	![anh_7](demo/anh_7.png)
+
--- app/src/main/java/com/example/imagecrop/MainActivity.java
+++ app/src/main/java/com/example/imagecrop/MainActivity.java
@@ -3,10 +3,14 @@ package com.example.imagecrop;
 import android.Manifest;
 import android.annotation.SuppressLint;
 import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
 import android.graphics.drawable.shapes.OvalShape;
+import android.media.MediaScannerConnection;
 import android.net.Uri;
+import android.os.Environment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -19,12 +23,26 @@ import android.widget.Toast;
 import com.theartofdev.edmodo.cropper.CropImage;
 import com.theartofdev.edmodo.cropper.CropImageView;
 
+import java.io.BufferedInputStream;
+import java.io.BufferedOutputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
+
 public class MainActivity extends AppCompatActivity {
 
     Button saveButton;
     ImageView imageview;
+    CropImageView cropImageView;
 
-    private Uri mCropImageUri;
+    private static final String IMAGE_DIRECTORY_NAME = "Image cropped";
+    private Uri mCropImageUri, croppedImageUri;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -50,7 +68,57 @@ public class MainActivity extends AppCompatActivity {
 
 
     public void onSaveImage(View v){
-        Toast.makeText(this, "Đã lưu", Toast.LENGTH_LONG).show();
+        File mediaStorageDir = new File(
+                Environment
+                        .getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES),
+                IMAGE_DIRECTORY_NAME);
+
+        if (!mediaStorageDir.exists()) {
+            if (!mediaStorageDir.mkdirs()) {
+                Log.d(IMAGE_DIRECTORY_NAME, "Tao thu muc that bai "
+                        + IMAGE_DIRECTORY_NAME + " directory");
+            }
+        }
+
+        FileOutputStream outputStream;
+        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss",
+                Locale.getDefault()).format(new Date());
+
+        String sourceFilename= croppedImageUri.getPath();
+        String destinationFilename = mediaStorageDir.getPath() + File.separator
+                + "IMG_" + timeStamp + ".jpg";
+
+        BufferedInputStream bis = null;
+        BufferedOutputStream bos = null;
+        try {
+            bis = new BufferedInputStream(new FileInputStream(sourceFilename));
+            bos = new BufferedOutputStream(new FileOutputStream(destinationFilename, false));
+            byte[] buf = new byte[1024];
+            bis.read(buf);
+            do {
+                bos.write(buf);
+            } while(bis.read(buf) != -1);
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                if (bis != null) bis.close();
+                if (bos != null) bos.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+
+        reloadToAlbum(destinationFilename);
+        Toast.makeText(this, "Da luu", Toast.LENGTH_LONG).show();
+    }
+    public void reloadToAlbum(String filePath){
+        MediaScannerConnection.scanFile(this,
+                new String[] { filePath }, null,
+                new MediaScannerConnection.OnScanCompletedListener() {
+                    public void onScanCompleted(String path, Uri uri) {
+
+                    }});
     }
     public void onSelectImageClick(View view) {
         CropImage.startPickImageActivity(this);
@@ -74,8 +142,10 @@ public class MainActivity extends AppCompatActivity {
         if (requestCode == CropImage.CROP_IMAGE_ACTIVITY_REQUEST_CODE) {
             CropImage.ActivityResult result = CropImage.getActivityResult(data);
             if (resultCode == RESULT_OK) {
-                ((ImageButton) findViewById(R.id.quick_start_cropped_image)).setImageURI(result.getUri());
-                Toast.makeText(this, "Crop thành công", Toast.LENGTH_LONG).show();
+                ((ImageView) findViewById(R.id.imageview)).setImageURI(result.getUri());
+                ((ImageButton) findViewById(R.id.imageButton)).setVisibility(View.GONE);
+                croppedImageUri = result.getUri();
+//                Toast.makeText(this, "Crop thành công"+ result.getUri(), Toast.LENGTH_LONG).show();
             } else if (resultCode == CropImage.CROP_IMAGE_ACTIVITY_RESULT_ERROR_CODE) {
                 Toast.makeText(this, "Crop thất bại: " + result.getError(), Toast.LENGTH_LONG).show();
             }
@@ -85,7 +155,6 @@ public class MainActivity extends AppCompatActivity {
     @Override
     public void onRequestPermissionsResult(int requestCode, String permissions[], int[] grantResults) {
         if (mCropImageUri != null && grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-            // required permissions granted, start crop image activity
             startCropImageActivity(mCropImageUri);
         } else {
             Toast.makeText(this, "Yêu cầu truy cập", Toast.LENGTH_LONG).show();
